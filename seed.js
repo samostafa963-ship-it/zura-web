89c481f1dcfd48beb65cc84a80672a4e8f11b18d
samostafa963-ship-it <samostafa963@gmail.com>
2026-06-02 17:27:25 +0300

    fix source URI

--- seed.js
+++ seed.js
@@ -1,12 +1,8 @@
 ﻿const { MongoClient } = require('mongodb');
-
-const SOURCE_URI = 'mongodb+srv://samostafa963:zura2025@cluster0.utximqz.mongodb.net/ZAD_Database?appName=Cluster0';
-const TARGET_URI = process.env.MONGO_URI || 'mongodb+srv://samostafa963:zura2025@cluster0.utximqz.mongodb.net/zura_web?appName=Cluster0';
-
+const SOURCE_URI = 'mongodb+srv://samostafa963:zadstor@cluster0.utximqz.mongodb.net/ZAD_Database?retryWrites=true&w=majority';
+const TARGET_URI = 'mongodb+srv://samostafa963:zura2025@cluster0.utximqz.mongodb.net/zura_web?appName=Cluster0';
 const COLLECTIONS = ['products', 'categories', 'banners'];
-
 async function seed() {
-  console.log('TARGET_URI:', TARGET_URI);
   const source = new MongoClient(SOURCE_URI);
   const target = new MongoClient(TARGET_URI);
   try {
@@ -14,10 +10,7 @@ async function seed() {
     await target.connect();
     console.log('Connected!');
     const sourceDb = source.db('ZAD_Database');
-    // extract db name from URI
-    const dbName = TARGET_URI.split('/').pop()?.split('?')[0] || 'zura_web';
-    console.log('Target DB:', dbName);
-    const targetDb = target.db(dbName);
+    const targetDb = target.db('zura_web');
     for (const col of COLLECTIONS) {
       const docs = await sourceDb.collection(col).find({}).toArray();
       await targetDb.collection(col).deleteMany({});
@@ -28,5 +21,4 @@ async function seed() {
   } catch(e) { console.error(e.message); }
   finally { await source.close(); await target.close(); process.exit(0); }
 }
-
 seed();
