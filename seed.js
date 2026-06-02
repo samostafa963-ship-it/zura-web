6c60a431ffa36764b4ebdcce4646881151cb3c95
samostafa963-ship-it <samostafa963@gmail.com>
2026-06-02 06:22:41 +0300

    add seed script

---
+++ seed.js
@@ -0,0 +1,24 @@
+﻿const { MongoClient } = require('mongodb');
+const SOURCE_URI = 'mongodb+srv://samostafa963:zura2025@cluster0.utximqz.mongodb.net/ZAD_Database?appName=Cluster0';
+const TARGET_URI = 'mongodb+srv://samostafa963:zura2025@cluster0.utximqz.mongodb.net/Zura_Web?appName=Cluster0';
+const COLLECTIONS = ['products', 'categories', 'banners'];
+async function seed() {
+  const source = new MongoClient(SOURCE_URI);
+  const target = new MongoClient(TARGET_URI);
+  try {
+    await source.connect();
+    await target.connect();
+    console.log('Connected!');
+    const sourceDb = source.db('ZAD_Database');
+    const targetDb = target.db('Zura_Web');
+    for (const col of COLLECTIONS) {
+      const docs = await sourceDb.collection(col).find({}).toArray();
+      await targetDb.collection(col).deleteMany({});
+      if (docs.length > 0) await targetDb.collection(col).insertMany(docs);
+      console.log(col + ': ' + docs.length);
+    }
+    console.log('Done!');
+  } catch(e) { console.error(e.message); }
+  finally { await source.close(); await target.close(); process.exit(); }
+}
+seed();
